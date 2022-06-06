d79abae6db2a709178fe315679ba3b238f4693a0
Noni Yohana <noniyohana28@gmail.com>
2022-06-06 18:28:56 +0700

    Initial commit

---
+++ app/src/main/java/com/noni/bottomnavigationnoni/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.noni.bottomnavigationnoni
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomView)
+        val navController = findNavController(R.id.fragment)
+        val appAppBarConfiguration = AppBarConfiguration(setOf(R.id.favFragment, R.id.musicFragment, R.id.newsFragment))
+        setupActionBarWithNavController(navController, appAppBarConfiguration)
+
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
